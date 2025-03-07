ea3cb098b71918b36e841acd0cd31de23bd491a8
Pavel Zubko <pts1701@yandex.ru>
2025-03-07 20:46:13 +0300

    First_try

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
-	...
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/personaldata"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/spentenergy"
 )
 
 const (
@@ -9,11 +16,37 @@ const (
 )
 
 // создайте структуру DaySteps
-...
-
+type DaySteps struct {
+	personaldata.Personal
+	Steps    int
+	Duration time.Duration
+}
 
 // создайте метод Parse()
-...
+func (ds *DaySteps) Parse(datastring string) (err error) {
+
+	s := strings.Split(datastring, ",")
+	if len(s) != 2 {
+		return errors.New("Некорректное колличество введенных данных")
+	}
+	steps, err := strconv.Atoi(s[0])
+	if err != nil {
+		return fmt.Errorf("ошибка колличества шагов: %v", err)
+	}
+	ds.Steps = steps
+
+	duration, err := time.ParseDuration(s[1])
+	if err != nil {
+		return fmt.Errorf("ошибка длительности тренировки: %v", err)
+	}
+
+	ds.Duration = duration
+	return nil
+}
 
 // создайте метод ActionInfo()
-...
+func (ds DaySteps) ActionInfo() (string, error) {
+	calories := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	distance := (float64(ds.Steps) * StepLength) / 1000
+	return fmt.Sprintf("Количество шагов:%d\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", ds.Steps, distance, calories), nil
+}
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,8 @@
 package spentenergy
 
-import ...
+import (
+	"time"
+)
 
 // Основные константы, необходимые для расчетов.
 const (
@@ -21,15 +23,18 @@ const (
 // WalkingSpentCalories возвращает количество потраченных калорий при ходьбе.
 //
 // Параметры:
-//
+
 // steps int - количество шагов.
 // weight float64 — вес пользователя.
 // height float64 — рост пользователя.
 // duration time.Duration — длительность тренировки.
 //
 // Создайте функцию ниже.
-...
-
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) float64 {
+	mean := МeanSpeed(steps, duration)
+	calories := ((walkingCaloriesWeightMultiplier * weight) + (mean*mean/height)*walkingSpeedHeightMultiplier) * float64(duration) * minInH
+	return calories
+}
 
 // Константы для расчета калорий, расходуемых при беге.
 const (
@@ -46,8 +51,12 @@ const (
 // duration time.Duration — длительность тренировки.
 //
 // Создайте функцию ниже.
-...
+func RunningSpentCalories(steps int, weight float64, duration time.Duration) float64 {
+	mean := МeanSpeed(steps, duration)
+	calories := (runningCaloriesMeanSpeedMultiplier * mean) - runningCaloriesMeanSpeedShift*weight
+	return calories
 
+}
 
 // МeanSpeed возвращает значение средней скорости движения во время тренировки.
 //
@@ -55,10 +64,16 @@ const (
 //
 // steps int — количество совершенных действий(число шагов при ходьбе и беге).
 // duration time.Duration — длительность тренировки.
-// 
+//
 // Создайте функцию ниже.
-...
-
+func МeanSpeed(steps int, duration time.Duration) float64 {
+	if duration < 0 {
+		return 0
+	}
+	dist := Distance(steps)
+	mean := dist / duration.Hours()
+	return mean
+}
 
 // Distance возвращает дистанцию(в километрах), которую преодолел пользователь за время тренировки.
 //
@@ -66,8 +81,9 @@ const (
 // Параметры:
 //
 // steps int — количество совершенных действий (число шагов при ходьбе и беге).
-// 
+//
 // Создайте функцию ниже
-...
-
-
+func Distance(steps int) float64 {
+	dist := (float64(steps) * lenStep) / mInKm
+	return dist
+}
